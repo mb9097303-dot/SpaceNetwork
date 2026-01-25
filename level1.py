48b35d973070e4f05cef85360273efc549d180c4
moishy barminka <mb9097303@gmail.com>
2026-01-25 12:33:54 +0200

    add instance from SpaceNetwork

--- level1.py
+++ level1.py
@@ -4,4 +4,9 @@ from space_network_lib import SpaceEntity, SpaceNetwork, Packet
 class Satellite(SpaceEntity):
     def receive_signal(self, packet: Packet):
         print(f"{self.name} Received: {packet}")
-        
+
+network = SpaceNetwork(level=1)
+sat1 = Satellite("Sat1",100)
+sat2 = Satellite("Sat2", 200)
+pack = Packet("The situation on the satellite",sat1,sat2)
+network.send(pack)
